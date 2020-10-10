8c15f7ecfac24bbbf316f64d85f6253b24088e8f
Lorenzo (Mec-iS) <tunedconsulting@gmail.com>
2020-10-10 18:55:27 +0100

    Implement test_indices (debug version)

--- src/model_selection/mod.rs
+++ src/model_selection/mod.rs
@@ -81,6 +81,105 @@ pub fn train_test_split<T: RealNumber, M: Matrix<T>>(
     (x_train, x_test, y_train, y_test)
 }
 
+
+///
+/// KFold Cross-Validation
+///
+/// Entities involved in the KFold procedure:
+///     * a dataset
+///     * a number k of groups (k-folds)
+/// 
+/// Procedure in `cross_validate()`: 
+///   1. Shuffle the dataset randomly.
+///   2. Split the dataset into k groups
+///   3. For each unique group:
+///         1. Take the group as a hold out or test data set
+///         2. Take the remaining groups as a training data set
+///         3. Fit a model on the training set and evaluate it on the test set
+///         4. Retain the evaluation score and discard the model
+///   4. Summarize the skill of the model using the sample of model evaluation scores
+trait BaseKFold {
+    /// Returns integer indices corresponding to test sets
+    fn test_indices<T: RealNumber, M: Matrix<T>>(&self, x: &M) -> Vec<usize>;
+
+    // /// Return matrix corresponding to test sets
+    // fn test_matrices<T: RealNumber, M: Matrix<T>>(&self, X: &M) -> Vec<&M>;
+
+    // /// Return a tuple containing the the training set indices for that split and
+    // /// the testing set indices for that split.
+    // fn split<T: RealNumber, M: Matrix<T>>(&self, X: &M) -> Vec<(Vec<usize>, Vec<usize>)>;
+}
+
+/// An implementation of KFold
+pub struct KFold {
+    n_splits: i32,
+    // TODO: to be implemented later
+    // shuffle: bool,
+    // random_state: i32, 
+}
+
+impl BaseKFold for KFold {
+    fn test_indices<T: RealNumber, M: Matrix<T>>(&self, x: &M) -> Vec<usize> {
+        // n_samples = _num_samples(X)  # number of sample in an array-like
+        let n_samples: usize = x.get_row_as_vec(0 as usize).len();
+        println!("n {:?}", &n_samples);
+
+        // indices = np.arange(n_samples)  # an iterator of size len(x)
+        let indices: Vec<usize> = (0..n_samples).collect();
+        println!("starting indices{:?}", &indices);
+
+        //  Return a new array of given shape n_split, filled with each element of n_samples divided by n_splits.
+        // fold_sizes = np.full(n_splits, n_samples // n_splits, dtype=int)
+        let mut fold_sizes = vec![n_samples / self.n_splits as usize; self.n_splits as usize];
+        println!("fold size {:?}", &fold_sizes);
+
+        // fold_sizes[:n_samples % n_splits] += 1
+        for i in 0..(n_samples % self.n_splits as usize) {
+            fold_sizes[i] = fold_sizes[i] + 1;
+        }
+        println!("fold size after correction{:?}", &fold_sizes);
+
+        let mut return_values: Vec<usize> = Vec::new();
+        let mut current: usize = 0;
+        for fold_size in fold_sizes {
+            let stop = current + fold_size;
+            println!("current, stop {:?}, {:?}", &current, &stop);
+            return_values.extend_from_slice(
+                &indices[current..stop].to_vec()
+            );
+            println!("loop {:?}", &return_values);
+            current = stop
+        }
+        
+        return return_values;
+
+    }
+
+    // fn test_matrices<T: RealNumber, M: Matrix<T>>(&self, X: &M) -> Vec<&M> {
+    //     // Python implementation
+    //     // for test_index in self._iter_test_indices(X, y, groups):
+    //     //     test_mask = np.zeros(_num_samples(X), dtype=bool)
+    //     //     test_mask[test_index] = True
+    //     //     yield test_mask
+    //     let tmp = vec![&M::zeros(2, 2)];
+    //     return tmp;
+
+    // }
+
+    // fn split<T: RealNumber, M: Matrix<T>>(&self, X: &M) -> Vec<(Vec<usize>, Vec<usize>)> {
+    //     // Python implementation
+    //     // X, y, groups = indexable(X, y, groups)
+    //     // indices = np.arange(_num_samples(X))  // an iterator of len(x)
+    //     // for test_index in self._iter_test_masks(X, y, groups):
+    //     //     train_index = indices[np.logical_not(test_index)]
+    //     //     test_index = indices[test_index]
+    //     //     yield train_index, test_index
+    //     let tmp = vec![(vec![0, 1], vec![0, 1])];
+    //     return tmp;
+    // }
+}
+
+
 #[cfg(test)]
 mod tests {
 
@@ -106,44 +205,28 @@ mod tests {
         assert_eq!(x_train.shape().0, y_train.len());
         assert_eq!(x_test.shape().0, y_test.len());
     }
-}
 
+    #[test]
+    fn run_kfold_return_test_indices_simple() {
+        let k = KFold { n_splits: 3};
 
-///
-/// KFold Cross-Validation
-///
-/// Entities involved in the KFold procedure:
-///     * a dataset
-///     * a number k of groups (k-folds)
-/// 
-/// Procedure in `cross_validate()`: 
-///   1. Shuffle the dataset randomly.
-///   2. Split the dataset into k groups
-///   3. For each unique group:
-///         1. Take the group as a hold out or test data set
-///         2. Take the remaining groups as a training data set
-///         3. Fit a model on the training set and evaluate it on the test set
-///         4. Retain the evaluation score and discard the model
-///   4. Summarize the skill of the model using the sample of model evaluation scores
-pub trait BaseKFold {
-    /// Return a tuple containing the the training set indices for that split and
-    /// the testing set indices for that split.
-    fn split(&self, X: Matrix) -> Iterator< Item = Tuple(ndarray::ArrayBase, ndarray::ArrayBase)>;
+        let x: DenseMatrix<f64> = DenseMatrix::rand(33, 33);
 
-    /// Returns integer indices corresponding to test sets
-    fn test_indices(&self, X: Matrix) -> Iterator< Item = i32>;
+        let test_indices = k.test_indices(&x);
 
-    /// Return matrix corresponding to test sets
-    fn test_matrices(&self, X: Matrix) -> Iterator< Item = Matrix>;
+        println!("{:?}", &test_indices);
+        assert_eq!(test_indices, (0..33).collect::<Vec<usize>>())
+    }
 
-}
+    #[test]
+    fn run_kfold_return_test_indices_odd() {
+        let k = KFold { n_splits: 3};
 
-pub struct KFold {
-    n_splits: i32,
-    shuffle: bool,
-    random_state: i32, 
-}
+        let x: DenseMatrix<f64> = DenseMatrix::rand(34, 34);
 
-impl BaseKFold for KFold {
-    ...
+        let test_indices = k.test_indices(&x);
+
+        println!("{:?}", &test_indices);
+        assert_eq!(test_indices, (0..34).collect::<Vec<usize>>())
+    }
 }
