f91b1f99425789b6d11c10941b079b4cd7150f5c
gaxler <g.axler@gmail.com>
2021-01-27 19:37:54 -0800

    fit SeriesOneHotEncoders to predefined columns

--- src/preprocessing/categorical_encoders.rs
+++ src/preprocessing/categorical_encoders.rs
@@ -25,3 +25,45 @@ pub struct OneHotEncoder {
     categorical_param_idxs: Vec<usize>,
 }
 
+impl<T: RealNumber, M: Matrix<T>> OneHotEncoder {
+    /// PlaceHolder
+
+    pub fn fit(data: &M, params: OneHotEncoderParams) -> Result<OneHotEncoder, Failed> {
+        match (params.categorical_param_idxs, params.infer_categorical) {
+            (None, false) => Err(Failed::fit(
+                "Must pass categorical series ids or infer flag",
+            )),
+
+            (Some(idxs), true) => Err(Failed::fit(
+                "Ambigous parameters, got both infer and categroy ids",
+            )),
+
+            (Some(idxs), false) => Ok(Self {
+                series_encoders: Self::build_series_encoders::<T, M>(data, &idxs[..]),
+                categorical_param_idxs: idxs,
+            }),
+
+            (None, true) => {
+                todo!("implement categorical auto-inference")
+            }
+        }
+    }
+
+    fn build_series_encoders(data: &M, idxs: &[usize]) -> Vec<SeriesOneHotEncoder<HashableReal>> {
+        let (nrows, _) = data.shape();
+        // let mut res: Vec<SeriesOneHotEncoder<HashableReal>> = Vec::with_capacity(idxs.len());
+        let mut tmp_col: Vec<T> = Vec::with_capacity(nrows);
+
+        let res: Vec<SeriesOneHotEncoder<HashableReal>> = idxs
+            .iter()
+            .map(|&idx| {
+                data.copy_col_as_vec(idx, &mut tmp_col);
+                let hashable_col = tmp_col.iter().map(|v| hashable_num::<T>(v));
+                SeriesOneHotEncoder::fit_to_iter(hashable_col)
+            })
+            .collect();
+        res
+    }
+
+
+}
