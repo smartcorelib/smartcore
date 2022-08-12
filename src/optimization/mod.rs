88713b4e8603590663c5c50d6e1d10e03b823b8e
Chris McComb <ccmcc2012@gmail.com>
2022-08-12 16:50:29 -0400

    another `rustfmt` fix - this is what I get for trying to make a quick fix!

--- src/optimization/mod.rs
+++ src/optimization/mod.rs
@@ -5,7 +5,7 @@ pub type F<'a, T, X> = dyn for<'b> Fn(&'b X) -> T + 'a;
 pub type DF<'a, X> = dyn for<'b> Fn(&'b mut X, &'b X) + 'a;
 
 #[allow(clippy::upper_case_acronyms)]
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum FunctionOrder {
     SECOND,
     THIRD,
