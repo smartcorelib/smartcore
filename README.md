e99bd7057891e807891b50f3350f55df5d3b7ee9
Lorenzo (Mec-iS) <tunedconsulting@gmail.com>
2022-10-13 19:37:32 +0100

    Improve developers walkthrough

--- README.md
+++ README.md
@@ -20,7 +20,7 @@
 ## Developers
 Contributions welcome, please start from [CONTRIBUTING and other relevant files](.github/CONTRIBUTING.md).
 
-### Basics
+### Walkthrough: traits system and basic structures
 
 #### numbers
 The library is founded on basic traits provided by `num-traits`. Basic traits are in `src/numbers`. These traits are used to define all the procedures in the library to make everything safer and provide constraints to what implementations can handle.
@@ -32,11 +32,27 @@ The library is founded on basic traits provided by `num-traits`. Basic traits ar
 * *matrix*: This provides the main entrypoint to matrices operations and currently the only structure provided in the shape of `struct DenseMatrix`. A matrix can be instantiated and automatically make available all the traits in "arrays" (sparse matrices implementation will be provided).
 * *vector*: Convenience traits are implemented for `std::Vec` to allow extensive reuse.
 
+These are all traits and by definition they do not allow instantiation. For instantiable structures see implementation like `DenseMatrix` with relative constructor.
+
 #### linalg/traits
 The traits in `src/linalg/traits` are closely linked to Linear Algebra's theoretical framework. These traits are used to specify characteristics and constraints for types accepted by various algorithms. For example these allow to define if a matrix is `QRDecomposable` and/or `SVDDecomposable`. See docstring for referencese to theoretical framework.
 
+As above these are all traits and by definition they do not allow instantiation. They are mostly used to provide constraints for implementations. For example, the implementation for Linear Regression requires the input data `X` to be in `smartcore`'s trait system `Array2<FloatNumber> + QRDecomposable<TX> + SVDDecomposable<TX>`, a 2-D matrix that is both QR and SVD decomposable; that is what the provided strucure `linalg::arrays::matrix::DenseMatrix` happens to be: `impl<T: FloatNumber> QRDecomposable<T> for DenseMatrix<T> {};impl<T: FloatNumber> SVDDecomposable<T> for DenseMatrix<T> {}`. 
+
 #### metrics
 Implementations for metrics (classification, regression, cluster, ...) and distance measure (Euclidean, Hamming, Manhattan, ...). For example: `Accuracy`, `F1`, `AUC`, `Precision`, `R2`. As everything else in the code base, these implementations reuse `numbers` and `linalg` traits and structures.
+
+These are collected in structures like `pub struct ClassificationMetrics<T> {}` that implements `metrics::Metrics`, these are groups of functions (classification, regression, cluster, ...) that provide instantiation for the structures. Each of those instantiation can be passed around using the relative function, like `pub fn accuracy<T: Number + RealNumber + FloatNumber, V: ArrayView1<T>>(y_true: &V, y_pred: &V) -> T`. This provides a mechanism for metrics to be passed to higher interfaces like the `cross_validate`:
+```rust
+let results =
+  cross_validate(
+      BiasedEstimator::fit,  // custom estimator
+      &x, &y,                // input data
+      NoParameters {},       // extra parameters
+      cv,                    // type of cross validator
+      &accuracy              // **metrics function** <--------
+  ).unwrap();
+```
 
 
 TODO: complete for all modules
--- benches/fastpair.rs
+++ benches/fastpair.rs
@@ -1,56 +1,56 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+// use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
-// to run this bench you have to change the declaraion in mod.rs ---> pub mod fastpair;
-use smartcore::algorithm::neighbour::fastpair::FastPair;
-use smartcore::linalg::basic::arrays::matrix::densematrix;
-use std::time::Duration;
+// // to run this bench you have to change the declaraion in mod.rs ---> pub mod fastpair;
+// use smartcore::algorithm::neighbour::fastpair::FastPair;
+// use smartcore::linalg::basic::arrays::matrix::densematrix;
+// use std::time::Duration;
 
-fn closest_pair_bench(n: usize, m: usize) -> () {
-    let x = DenseMatrix::<f64>::rand(n, m);
-    let fastpair = FastPair::new(&x);
-    let result = fastpair.unwrap();
+// fn closest_pair_bench(n: usize, m: usize) -> () {
+//     let x = DenseMatrix::<f64>::rand(n, m);
+//     let fastpair = FastPair::new(&x);
+//     let result = fastpair.unwrap();
 
-    result.closest_pair();
-}
+//     result.closest_pair();
+// }
 
-fn closest_pair_brute_bench(n: usize, m: usize) -> () {
-    let x = DenseMatrix::<f64>::rand(n, m);
-    let fastpair = FastPair::new(&x);
-    let result = fastpair.unwrap();
+// fn closest_pair_brute_bench(n: usize, m: usize) -> () {
+//     let x = DenseMatrix::<f64>::rand(n, m);
+//     let fastpair = FastPair::new(&x);
+//     let result = fastpair.unwrap();
 
-    result.closest_pair_brute();
-}
+//     result.closest_pair_brute();
+// }
 
-fn bench_fastpair(c: &mut Criterion) {
-    let mut group = c.benchmark_group("FastPair");
+// fn bench_fastpair(c: &mut Criterion) {
+//     let mut group = c.benchmark_group("FastPair");
 
-    // with full samples size (100) the test will take too long
-    group.significance_level(0.1).sample_size(30);
-    // increase from default 5.0 secs
-    group.measurement_time(Duration::from_secs(60));
+//     // with full samples size (100) the test will take too long
+//     group.significance_level(0.1).sample_size(30);
+//     // increase from default 5.0 secs
+//     group.measurement_time(Duration::from_secs(60));
 
-    for n_samples in [100_usize, 1000_usize].iter() {
-        for n_features in [10_usize, 100_usize, 1000_usize].iter() {
-            group.bench_with_input(
-                BenchmarkId::from_parameter(format!(
-                    "fastpair --- n_samples: {}, n_features: {}",
-                    n_samples, n_features
-                )),
-                n_samples,
-                |b, _| b.iter(|| closest_pair_bench(*n_samples, *n_features)),
-            );
-            group.bench_with_input(
-                BenchmarkId::from_parameter(format!(
-                    "brute --- n_samples: {}, n_features: {}",
-                    n_samples, n_features
-                )),
-                n_samples,
-                |b, _| b.iter(|| closest_pair_brute_bench(*n_samples, *n_features)),
-            );
-        }
-    }
-    group.finish();
-}
+//     for n_samples in [100_usize, 1000_usize].iter() {
+//         for n_features in [10_usize, 100_usize, 1000_usize].iter() {
+//             group.bench_with_input(
+//                 BenchmarkId::from_parameter(format!(
+//                     "fastpair --- n_samples: {}, n_features: {}",
+//                     n_samples, n_features
+//                 )),
+//                 n_samples,
+//                 |b, _| b.iter(|| closest_pair_bench(*n_samples, *n_features)),
+//             );
+//             group.bench_with_input(
+//                 BenchmarkId::from_parameter(format!(
+//                     "brute --- n_samples: {}, n_features: {}",
+//                     n_samples, n_features
+//                 )),
+//                 n_samples,
+//                 |b, _| b.iter(|| closest_pair_brute_bench(*n_samples, *n_features)),
+//             );
+//         }
+//     }
+//     group.finish();
+// }
 
-criterion_group!(benches, bench_fastpair);
-criterion_main!(benches);
+// criterion_group!(benches, bench_fastpair);
+// criterion_main!(benches);
