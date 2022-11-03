ed9769f651ed55541482d732d2cd93474350c4ce
Lorenzo <tunedconsulting@gmail.com>
2022-11-03 15:49:00 +0000

    Implement CSV reader with new traits (#209)

--- src/svm/mod.rs
+++ src/svm/mod.rs
@@ -23,9 +23,10 @@
 //! <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 //! <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 /// search parameters
-pub mod search;
 pub mod svc;
 pub mod svr;
+// /// search parameters space
+// pub mod search;
 
 use core::fmt::Debug;
 use std::marker::PhantomData;
