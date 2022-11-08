8e6e5f9e68d33ff906acd1f37f9eb38e5e33c7a5
Lorenzo (Mec-iS) <tunedconsulting@gmail.com>
2022-11-08 11:47:31 +0000

    Use getrandom as default (for no-std feature)

--- src/rand_custom.rs
+++ src/rand_custom.rs
@@ -1,5 +1,7 @@
 #[cfg(not(feature = "std"))]
 pub(crate) use rand::rngs::SmallRng as RngImpl;
+#[cfg(not(feature = "std"))]
+use getrandom;
 #[cfg(feature = "std")]
 pub(crate) use rand::rngs::StdRng as RngImpl;
 use rand::SeedableRng;
@@ -13,7 +15,10 @@ pub(crate) fn get_rng_impl(seed: Option<u64>) -> RngImpl {
                     use rand::RngCore;
                     RngImpl::seed_from_u64(rand::thread_rng().next_u64())
                 } else {
-                    panic!("seed number needed for non-std build");
+                    // non-std build, use getrandom
+                    let mut buf = [0u8; 64];
+                    getrandom::getrandom(&mut buf).unwrap();
+                    RngImpl::seed_from_u64(buf[0] as u64)
                 }
             }
         }
