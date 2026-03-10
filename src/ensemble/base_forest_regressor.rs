c77781b81df0c5e72b1f2f3392b623a3756ed09a
Andrey Shevchenko <44893410+skywardfire1@users.noreply.github.com>
2026-03-10 19:36:55 +0300

    added Jaccard distance (#358)
    * added Jaccard distance
    * two encounters of a bad pattern is_none() + unwrap(). FIXED.
    * added 3 tests, incl. symmetry test. Now 4 in total.

--- src/ensemble/base_forest_regressor.rs
+++ src/ensemble/base_forest_regressor.rs
@@ -161,25 +161,31 @@ impl<TX: Number + FloatNumber + PartialOrd, TY: Number, X: Array2<TX>, Y: Array1
     /// Predict OOB classes for `x`. `x` is expected to be equal to the dataset used in training.
     pub fn predict_oob(&self, x: &X) -> Result<Y, Failed> {
         let (n, _) = x.shape();
-        if self.samples.is_none() {
-            Err(Failed::because(
-                FailedError::PredictFailed,
-                "Need samples=true for OOB predictions.",
-            ))
-        } else if self.samples.as_ref().unwrap()[0].len() != n {
-            Err(Failed::because(
+
+        let samples = match &self.samples {
+            Some(s) => s,
+            None => {
+                return Err(Failed::because(
+                    FailedError::PredictFailed,
+                    "Need samples=true for OOB predictions.",
+                ))
+            }
+        };
+
+        if samples[0].len() != n {
+            return Err(Failed::because(
                 FailedError::PredictFailed,
                 "Prediction matrix must match matrix used in training for OOB predictions.",
-            ))
-        } else {
-            let mut result = Y::zeros(n);
-
-            for i in 0..n {
-                result.set(i, self.predict_for_row_oob(x, i));
-            }
-
-            Ok(result)
+            ));
         }
+
+        let mut result = Y::zeros(n);
+
+        for i in 0..n {
+            result.set(i, self.predict_for_row_oob(x, i));
+        }
+
+        Ok(result)
     }
 
     fn predict_for_row_oob(&self, x: &X, row: usize) -> TY {
