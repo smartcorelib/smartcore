d3ea80f21e0c366cf5e1604ef6d539a8f037190d
Lorenzo (Mec-iS) <tunedconsulting@gmail.com>
2026-07-08 20:00:14 +0100

    style(ndarray): sort imports alphabetically

--- src/linalg/ndarray/matrix.rs
+++ src/linalg/ndarray/matrix.rs
@@ -13,7 +13,7 @@ use crate::linalg::traits::svd::SVDDecomposable;
 use crate::numbers::basenum::Number;
 use crate::numbers::realnum::RealNumber;
 
-use ndarray::{s, Array, ArrayBase, ArrayView, ArrayViewMut, Axis, Ix2, OwnedRepr, Order};
+use ndarray::{s, Array, ArrayBase, ArrayView, ArrayViewMut, Axis, Ix2, Order, OwnedRepr};
 
 // ---------------------------------------------------------------------------
 // ArrayBase<OwnedRepr<T>, Ix2>  (owned 2-D array)
